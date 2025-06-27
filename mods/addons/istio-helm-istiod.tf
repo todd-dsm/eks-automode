4e511c8523eda6bb5ce4a2b791bac925e7b2cf86
Todd E Thomas <todd.dsm@gmail.com>
2025-06-26 17:21:27 -0700

    istiod installed

---
+++ mods/addons/istio-helm-istiod.tf
@@ -0,0 +1,45 @@
+########################################################################################################################
+# Istiod - Istio Control Plane for Ambient Mode
+# DOCS: https://istio.io/latest/docs/ambient/install/helm/
+########################################################################################################################
+resource "helm_release" "istiod" {
+  name       = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "istiod"
+  version    = "1.26.1"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+
+  # Static configuration from values file
+  values = [
+    file("${path.root}/addons/istio/values-istiod.yaml")
+  ]
+
+  # Dynamic Terraform-managed values
+  set = [
+    {
+      name  = "profile"
+      value = "ambient"
+    }
+  ]
+
+  # Installation settings
+  wait              = true
+  wait_for_jobs     = true
+  timeout           = 900 # Control plane can take longer
+  create_namespace  = false
+  dependency_update = true
+
+  # Lifecycle management
+  atomic          = true
+  cleanup_on_fail = true
+  replace         = false
+
+  depends_on = [
+    helm_release.istio_cni
+  ]
+
+  lifecycle {
+    ignore_changes  = [values]
+    prevent_destroy = false
+  }
+}
