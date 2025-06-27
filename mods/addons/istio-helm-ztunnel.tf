4e452f1fa3a89ca025c3e914df0b94e5a406ba38
Todd E Thomas <todd.dsm@gmail.com>
2025-06-26 17:46:20 -0700

    istio ztunnel installed

---
+++ mods/addons/istio-helm-ztunnel.tf
@@ -0,0 +1,45 @@
+########################################################################################################################
+# Ztunnel - Istio Ambient Mode Node Proxy (Data Plane)
+# DOCS: https://istio.io/latest/docs/ambient/install/helm/
+########################################################################################################################
+resource "helm_release" "ztunnel" {
+  name       = "ztunnel"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "ztunnel"
+  version    = "1.26.1"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+
+  # Static configuration from values file
+  values = [
+    file("${path.root}/addons/istio/values-ztunnel.yaml")
+  ]
+
+  #   # Dynamic Terraform-managed values
+  #   set = [
+  #     {
+  #       name  = "global.istioNamespace"
+  #       value = kubernetes_namespace.istio_system.metadata[0].name
+  #     },
+  #   ]
+
+  # Installation settings
+  wait              = true
+  wait_for_jobs     = true
+  timeout           = 600
+  create_namespace  = false
+  dependency_update = true
+
+  # Lifecycle management
+  atomic          = true
+  cleanup_on_fail = true
+  replace         = false
+
+  depends_on = [
+    helm_release.istiod
+  ]
+
+  lifecycle {
+    ignore_changes  = [values]
+    prevent_destroy = false
+  }
+}
